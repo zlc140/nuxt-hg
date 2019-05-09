bd06c5f62afc7cfc58d094c60afdedfebbaa3b17
zhanglucheng <zhanglucheng@houbank.com>
2019-05-09 13:51:53 +0800

    添加环境

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const IS_DEV = process.env.NODE_ENV !== 'production'
 
-const API_ROOT = IS_DEV ? 'http://sit.kypapp.in.houbank.net/kuaiyipai-app-api' : '';
+const API_ROOT = IS_DEV ? 'http://sit.kypapp.in.houbank.net/kuaiyipai-api' : 'http://uat.kypapp.in.houbank.net/kuaiyipai-api';
 
 const STATIC_PATH = 'https://static/';
 
